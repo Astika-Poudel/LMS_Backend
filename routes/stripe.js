9ba786fe9fb0aa3a0ffd9ca3c4157c60c25ad991
Astika-Poudel <np03cs4a220345@heraldcollege.edu.np>
2025-04-27 00:59:22 +0545

    Esewa payment

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createPaymentIntent } from "../controllers/payment.js";
+import { createPaymentIntent, initiateEsewaPayment, verifyEsewaPayment } from "../controllers/payment.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
-// Route to create a payment intent
+// Route to create a payment intent for Stripe
 router.post("/create-payment-intent", isAuth, createPaymentIntent);
+
+// eSewa Payment Initiation
+router.post("/initiate-esewa", isAuth, initiateEsewaPayment);
+
+// eSewa Payment Verification
+router.get("/verify-esewa", isAuth, verifyEsewaPayment);
 
 export default router;
