249680bc1e3744f87e9fa09465e277310b7b890c
Astika-Poudel <np03cs4a220345@heraldcollege.edu.np>
2025-05-01 22:31:04 +0545

    Updated enrollment

--- routes/enroll.js
+++ routes/enroll.js
@@ -4,10 +4,7 @@ import { enrollCourse, getEnrolledCourses } from "../controllers/enroll.js";
 
 const router = express.Router();
 
-// Route to enroll a user in a course
 router.post("/user/enroll/:courseId", isAuth, enrollCourse);
-
-// Route to get user's enrolled courses
 router.get("/user/enrolled", isAuth, getEnrolledCourses);
 
 export default router;
