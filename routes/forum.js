5785dd00409d70c5d0a9c90ae146745a2cd61f65
Astika-Poudel <np03cs4a220345@heraldcollege.edu.np>
2025-05-03 00:28:41 +0545

    Tag Routes

--- routes/forum.js
+++ routes/forum.js
@@ -1,5 +1,6 @@
+// routes/forumRouter.js
 import express from "express";
-import { getForumPosts, createForumPost, addComment, addReaction, getTaggableUsers } from "../controllers/forum.js";
+import { getForumPosts, createForumPost, addComment, addReaction, getTaggableUsers, updateForumPost, deleteForumPost } from "../controllers/forum.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
@@ -9,5 +10,7 @@ router.post("/forum/:courseId", isAuth, createForumPost);
 router.post("/forum/:courseId/:postId/comment", isAuth, addComment);
 router.post("/forum/:courseId/:postId/reaction", isAuth, addReaction);
 router.get("/forum/:courseId/users", isAuth, getTaggableUsers);
+router.put("/forum/:courseId/:postId", isAuth, updateForumPost);
+router.delete("/forum/:courseId/:postId", isAuth, deleteForumPost);
 
 export default router;
